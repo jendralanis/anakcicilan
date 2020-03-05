303fc8ad3bfa7fc178fffe62a525bc8830fdc99d
beksbhale20 <61486131+beksbhale20@users.noreply.github.com>
2020-03-05 15:10:49 +0700

    Update func.php

--- func.php
+++ func.php
@@ -182,7 +182,7 @@ function claims($token,$voc)
     }
     function claim2($token)
     {
-    $data = '{"promo_code":"G-7RCBDYN"}';    
+    $data = '{"promo_code":"G-43CGVQX"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
     if ($claim['success'] == 1)
         {
@@ -196,7 +196,7 @@ function claims($token,$voc)
     }
      function ride($token)
     {
-    $data = '{"promo_code":"G-75SR565"}';    
+    $data = '{"promo_code":"G-43CGVQX"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
     if ($claim['success'] == 1)
         {
@@ -210,7 +210,7 @@ function claims($token,$voc)
     }
      function cekvocer($token)
     {
-    $data = '{"promo_code":"G-75SR565"}';    
+    $data = '{"promo_code":"G-43CGVQX"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
     if ($claim['success'] == 1)
         {
@@ -224,7 +224,7 @@ function claims($token,$voc)
     }
      function pengen($token)
     {
-    $data = '{"promo_code":"G-FNF7B6D"}';    
+    $data = '{"promo_code":"G-43CGVQX"}';    
     $claim = request("/go-promotions/v1/promotions/enrollments", $token, $data);
     if ($claim['success'] == 1)
         {
@@ -239,7 +239,7 @@ function claims($token,$voc)
 
 function reff($token)
     {
-    $data = '{"referral_code":"G-75SR565"}';    
+    $data = '{"referral_code":"G-43CGVQX"}';    
     $claim = request("/customer_referrals/v1/campaign/enrolment", $token, $data);
     if ($claim['success'] == 1)
         {
